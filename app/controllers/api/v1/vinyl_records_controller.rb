8e0114f8722872b4de51ee2ea5186be5b92c7337
Jayce Higgins <jhiggins@instructure.com>
2016-04-12 17:05:09 -0600

    flesh out the crud actions for vinyl record

--- app/controllers/api/v1/vinyl_records_controller.rb
+++ app/controllers/api/v1/vinyl_records_controller.rb
@@ -1,10 +1,59 @@
 module API
   module V1
     class VinylRecordsController < ApplicationController
+      before_action :load_vinyl_record, except: %i{index new create}
+      skip_before_action :verify_authenticity_token
+
       def index
-        render json: {foo: "bar"}
+        render json: VinylRecord.all
       end
-      #add other routes!!!
+
+      def show
+        render json: @vinyl_record
+      end
+
+      def create
+        if vinyl_record = VinylRecord.create(pruned_attrs)
+          msg = { :status => "ok", :message => "Success!", :object => {id: vinyl_record.id} }
+        else
+          msg = { :status => "failure", :message => "Failed to create record #{pruned_attrs}" }
+        end
+
+        render json: msg
+      end
+
+      def update
+        updated_vinyl_record = @vinyl_record.update_attributes(pruned_attrs)
+
+        render json: updated_vinyl_record
+      end
+
+      def destroy
+        if @vinyl_record.destroy
+          msg = { :status => "ok", :message => "Success!" }
+        else
+          msg = { :status => "failure", :message => "Failed to delete record #{params[:id]}" }
+        end
+        render json: msg
+      end
+
+      private
+
+      def pruned_attrs
+        {
+          album: params[:album],
+          artist: params[:artist],
+          cost: params[:cost],
+          genre: params[:genre]
+        }
+      end
+
+      def load_vinyl_record
+        @vinyl_record = VinylRecord.find(params[:id])
+
+      rescue ActiveRecord::RecordNotFound
+        render json: { :status => "failure", :message => "Record not found with id #{params[:id]}"}
+      end
     end
   end
 end
